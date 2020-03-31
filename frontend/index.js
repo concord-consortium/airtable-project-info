d7e9a6eff872490c477d7fe53169234968182d6d
Scott Cytacki <scytacki@concord.org>
2020-03-30 20:58:41 -0400

    add budget-graph and dashboard mode
    Currently this just controls whether the project selection button is visible or not.

--- frontend/index.js
+++ frontend/index.js
@@ -7,6 +7,7 @@ import {
     ViewPickerSynced,
     Box,
     Select,
+    SelectSynced,
     FormField,
 } from '@airtable/blocks/ui';
 import React, {useState, useEffect} from 'react';
@@ -14,13 +15,13 @@ import ProjectBudgetGraph from './project-budget-graph';
 
 const GlobalConfigKeys = {
     VIEW_ID: 'viewId',
+    MODE: 'mode',
 };
 
 function ProjectInfoBlock() {
   const base = useBase();
   const globalConfig = useGlobalConfig();
   const [isSettingsVisible, setIsSettingsVisible] = useState(false);
-  const [project, setProject] = useState(null);
 
   useSettingsButton(() => setIsSettingsVisible(!isSettingsVisible));
 
@@ -28,14 +29,9 @@ function ProjectInfoBlock() {
   const viewId = globalConfig.get(GlobalConfigKeys.VIEW_ID);
   const view = table.getViewByIdIfExists(viewId);
 
-  const allRecords = useRecords(view);
+  const mode = globalConfig.get(GlobalConfigKeys.MODE);
 
-  const options = [{ value: null, label: 'All Projects'}];
-  if(allRecords) {
-    for(let record of allRecords){
-      options.push({ value: record.id, label: record.primaryCellValueAsString})
-    }
-  }
+  const allRecords = useRecords(view);
 
   // If there is no view set, then show the settings automatically
   useEffect(() => {
@@ -43,11 +39,6 @@ function ProjectInfoBlock() {
       setIsSettingsVisible(true);
     }
   }, [view])
-
-  let records = allRecords;
-  if(project && allRecords) {
-    records = [allRecords.find(record => record.id === project)];
-  }
 
   return (
       <Box
@@ -62,23 +53,60 @@ function ProjectInfoBlock() {
           {isSettingsVisible && (
               <Settings table={table} />
           )}
-          <Box width="33.33%" padding={1} marginBottom={0}>
-              <Select
-                options={options}
-                value={project}
-                onChange={newProject => setProject(newProject)}
-              />
-          </Box>
-          <ProjectBudgetGraph records={records}/>
+          {mode === "dashboard" && (
+            <ProjectDashboard records={allRecords}/>
+          )}
+          {mode === "budget-graph" && (
+            <ProjectBudgetGraph records={allRecords}/>
+          )}
       </Box>
   );
 }
 
+function ProjectDashboard({records}) {
+  const [project, setProject] = useState(null);
+
+  let options = [{value: null, label: "Choose a Project"}];
+  if(records) {
+    for(let record of records){
+      options.push({value: record.id, label: record.primaryCellValueAsString});
+    }
+  }
+
+  let projects = records;
+  if(project && records) {
+    projects = [records.find(record => record.id === project)];
+  }
+
+  return (
+    <Box>
+      <Box width="33.33%" padding={1} marginBottom={0}>
+          <Select
+            options={options}
+            value={project}
+            onChange={newProject => setProject(newProject)}
+          />
+      </Box>
+      {project && (
+        <ProjectBudgetGraph records={projects}/>
+      )}
+    </Box>
+  );
+}
+
 function Settings({table}) {
+    const modeOptions = [
+      { value: "dashboard", label: "Project Dashboard"},
+      { value: "budget-graph", label: "Project Budget Graph"}
+    ];
+
     return (
         <Box display="flex" padding={3} borderBottom="thick">
             <FormField label="View" width="33.33%" paddingX={1} marginBottom={0}>
                 <ViewPickerSynced table={table} globalConfigKey={GlobalConfigKeys.VIEW_ID} />
+            </FormField>
+            <FormField label="Mode" width="33.33%" paddingX={1} marginBottom={0}>
+                <SelectSynced options={modeOptions} globalConfigKey={GlobalConfigKeys.MODE} />
             </FormField>
         </Box>
     );
